c8f5adc05a9e8082853d48b5f4f238d5e7cc00d9
viniciusriosfuck <63118219+viniciusriosfuck@users.noreply.github.com>
2020-04-19 17:00:45 -0300

    Update legend and filename
    Altera legendas dos gráficos e nome dos arquivos de output

--- functions/plot_functions.py
+++ functions/plot_functions.py
@@ -3,35 +3,64 @@ from .utils import *
 
 
 def auxiliar_names(covid_parameters, model_parameters):
-    """
-    Provides filename and legend for plots from the sensitivity parameter analysis
-    """
-
-    alpha = covid_parameters.alpha
-    beta = covid_parameters.beta
-    gamma = covid_parameters.gamma
-
-    omega_i = model_parameters.contact_reduction_elderly
-    omega_j = model_parameters.contact_reduction_young
-
-    # AUTOMATIZACAO PARA ANALISE SENSIBILIDADE PARAMETROS
-    psel = 3                                        # 0 / 1 / 2 / 3
-    pvalue = (alpha, beta, gamma, omega_i)
-    pname = 'alpha', 'beta', 'gamma', 'omegaI'      # parametros
-    pInt = ("%.1f" % pvalue[psel])[0]               # parte inteira do parametro (1 caractere)
-    pDec = ("%.1f" % pvalue[psel])[2]               # parte decimal do parametro (1 caractere)
-    filename = pname[psel] + pInt + '_' + pDec
-    if psel == 3:
-        filename = filename + '__omegaJ' + ("%.1f" % omega_j)[0] + '_' + ("%.1f" % omega_j)[2]
-    leg = (
-        f'SEIR($alpha$={"%0.1f" % alpha})',
-        f'SEIR($\\beta$={"%0.1f" % beta})',
-        f'SEIR($\\gamma$={"%0.1f" % gamma})',
-        f'SEIR($\\omega_I$={"%0.1f" % omega_i}, $\\omega_J$={"%0.1f" % omega_j})'
-    )
-    legenda = leg[psel]
-
-    return filename, legenda
+	"""
+	Provides filename and legend for plots from the sensitivity parameter analysis
+	"""
+	
+	cp = covid_parameters
+	mp = model_parameters
+	
+	alpha = cp.alpha			# incubation_rate
+	beta = cp.beta				# infectiviy_rate
+	gamma = cp.gamma			# contamination_rate
+	
+	# Variaveis de apoio
+	# incubation_period = 1 / alpha
+	# infectiviy_period = 1 / beta
+	basic_reproduction_number = gamma / beta
+	r0 = basic_reproduction_number
+	
+	omega_i = mp.contact_reduction_elderly
+	omega_j = mp.contact_reduction_young
+	
+	Ei0 = mp.init_exposed_elderly				# initial exposed population old ones: 55+ years
+	Ej0 = mp.init_exposed_young					# initial exposed population young ones: 0-54 years
+	Ii0 = mp.init_infected_elderly				# initial infected population old ones: 55+ years
+	Ij0 = mp.init_infected_young				# initial infected population young ones: 0-54 years
+	Ri0 = mp.init_removed_elderly				# initial removed population old ones: 55+ years
+	Rj0 = mp.init_removed_young					# initial removed population young ones: 0-54 years
+	
+	# AUTOMATIZACAO PARA ANALISE SENSIBILIDADE PARAMETROS
+	#psel = 3                                        # 0 / 1 / 2 / 3
+	#pvalue = (alpha, beta, gamma, omega_i)
+	#pname = 'alpha', 'beta', 'gamma', 'wI'      # parametros
+	#pInt = ("%.1f" % pvalue[psel])[0]           # parte inteira do parametro (1 caractere)
+	#pDec = ("%.1f" % pvalue[psel])[2]           # parte decimal do parametro (1 caractere)
+	
+	filename = ('SEIR'
+	+ '_r' + ("%.1f" % r0)[0] + '_' + ("%.1f" % r0)[2]
+	+ '__g' + ("%.1f" % gamma)[0] + '_' + ("%.1f" % gamma)[2]
+	+ '__wI' + ("%.1f" % omega_i)[0] + '_' + ("%.1f" % omega_i)[2]
+	+ '__wJ' + ("%.1f" % omega_j)[0] + '_' + ("%.1f" % omega_j)[2]
+	)
+	
+	#filename = pname[psel] + pInt + '_' + pDec
+	#if psel == 3:
+		#filename = filename + '__wJ' + ("%.1f" % omega_j)[0] + '_' + ("%.1f" % omega_j)[2]
+	#leg = (
+	#	f'SEIR($alpha$={"%0.1f" % alpha})',
+	#	f'SEIR($\\beta$={"%0.1f" % beta})',
+	#	f'SEIR($\\gamma$={"%0.1f" % gamma})',
+	#	f'SEIR($\\omega_I$={"%0.1f" % omega_i}, $\\omega_J$={"%0.1f" % omega_j})'
+	#)
+	#legenda = leg[psel]
+	legenda = (
+		f'$\\alpha$={"%0.1f" % alpha}, $\gamma$={"%0.1f" % gamma}, $r_0$={"%0.1f" % r0}\n'
+		f'Idosos: $\\omega_e$={"%0.1f" % omega_i}, $E_{{e0}}$={Ei0}, $I_{{e0}}$={Ii0}, $R_{{e0}}$={Ri0}\n'
+		f'Jovens: $\\omega_y$={"%0.1f" % omega_j}, $E_{{y0}}$={Ej0}, $I_{{y0}}$={Ij0}, $R_{{y0}}$={Rj0}'
+	)
+	
+	return filename, legenda
 
 
 def plots(filename, legenda, results, demograph_parameters, model_parameters):
@@ -39,7 +68,8 @@ def plots(filename, legenda, results, demograph_parameters, model_parameters):
     """
     Makes two plots? 0) SEIR curve, 1) Hospital Demand
     """
-
+	
+    N = demograph_parameters.population
     capacidade_leitos = demograph_parameters.bed_ward
     capacidade_UTIs = demograph_parameters.bed_icu
 
@@ -49,8 +79,8 @@ def plots(filename, legenda, results, demograph_parameters, model_parameters):
 
     # plot
     tamfig = (8,6)
-    fsLabelTitle = 15   # Font Size: Label and Title
-    fsPlotLegend = 12   # Font Size: Plot and Legend
+    fsLabelTitle = 11   # Font Size: Label and Title
+    fsPlotLegend = 10   # Font Size: Plot and Legend
 
     # SEIR
     plt.figure(0)
@@ -60,7 +90,7 @@ def plots(filename, legenda, results, demograph_parameters, model_parameters):
         [['Si', 'Sj', 'Ei', 'Ej', 'Ii', 'Ij', 'Ri', 'Rj']]
         .plot(figsize=tamfig, fontsize=fsPlotLegend, logy=False)
      )
-    plt.title(f'Numero de Pessoas Atingidas com modelo:\n' + legenda, fontsize=fsLabelTitle)
+    plt.title(f'Pessoas atingidas com modelo SEIR: $N$={N}, ' + legenda, fontsize=fsLabelTitle)
     plt.legend(['Suscetiveis Idosas', 'Suscetiveis Jovens', 'Expostas Idosas', 'Expostas Jovens',
                 'Infectadas Idosas', 'Infectadas Jovens', 'Removidas Idosas', 'Removidas Jovens'],
                fontsize=fsPlotLegend)
@@ -87,7 +117,7 @@ def plots(filename, legenda, results, demograph_parameters, model_parameters):
     # plt.hlines(capacidade_UTIs*lotacao[2],1,t_max) #80%
     plt.hlines(capacidade_UTIs*lotacao[3], 1, t_max, label='100% UTI', colors='g', linestyles='dashed')  # 100%
 
-    plt.title(f'Demanda diaria de leitos:\n' + legenda, fontsize=fsLabelTitle)
+    plt.title(f'Demanda diaria de leitos: $N$={N}, ' + legenda, fontsize=fsLabelTitle)
     plt.legend(['Leito normal idosos', 'Leito normal jovens', 'UTI idosos',
                 'UTI jovens', '100% Leitos', '100% UTIs'], fontsize=fsPlotLegend)
     plt.xlabel('Dias', fontsize=fsLabelTitle)
